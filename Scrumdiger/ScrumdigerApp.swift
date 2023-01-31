892686a3945dce6cf092905843827583e6f527d2
vesniaa <pyssiki@yandex.ru>
2023-01-31 14:45:20 +0300

    Initial Commit

---
+++ Scrumdiger/ScrumdigerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdigerApp.swift
+//  Scrumdiger
+//
+//  Created by Евгения Аникина on 31.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdigerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
