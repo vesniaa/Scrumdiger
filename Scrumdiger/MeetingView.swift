ed0a99eac5ae484a7a8543d9ab2f5b8c2aa648d9
vesniaa <pyssiki@yandex.ru>
2023-01-31 15:01:00 +0300

    Label new

---
+++ Scrumdiger/MeetingView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  Scrumdiger
+//
+//  Created by Евгения Аникина on 31.01.2023.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            ProgressView(value: 5, total: 15)
+            HStack {
+                VStack {
+                    Text("Seconds Elapsed")
+                    Label("300", systemImage: "hourglass.bottomhalf.fill")
+                }
+                VStack {
+                    Text("Seconds Remaining")
+                }
+            }
+        }
+    }
+}
+
+struct MeetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView()
+    }
+}
