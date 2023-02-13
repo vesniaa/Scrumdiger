6be7b939e1f64f2b5ce06b8074579280cbbc8b1a
vesniaa <pyssiki@yandex.ru>
2023-02-13 14:11:41 +0300

    new private var

--- Scrumdiger/DetailEditView.swift
+++ Scrumdiger/DetailEditView.swift
@@ -8,8 +8,9 @@
 import SwiftUI
 
 struct DetailEditView: View {
-    //@State определяет источник истинности для типов значений
+    //@State определяет источник истинности для типов значений.
     @State private var data = DailyScrum.Data()
+    @State private var newAttendeeName = ""
     
     var body: some View {
         Form {
@@ -19,11 +20,21 @@ struct DetailEditView: View {
                     Slider(value: $data.lengthInMinutes, in: 5...30) //step: Text("Length"))
                     Spacer()
                     Text("\(Int(data.lengthInMinutes)) minutes")
-                    }
+                }
+            }
+            Section(header: Text("Attendees")) {
+                ForEach(data.attendees) { attendee in
+                    Text(attendee.name)
+                }
+                //модификатор onDelete, фраемворк вызывает замыкание для удаления участника группы,когда пользователь проводит пальцем чтобы удалить строку.
+                .onDelete { indices in
+                    data.attendees.remove(atOffsets: indices)
+                    
                 }
             }
         }
     }
+}
 
 
 
